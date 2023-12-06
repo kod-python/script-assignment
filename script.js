0b160fb1c649514ab083b605ec1f0df8d1b48ff4
kod-python <kelvinlik0245@gmail.com>
2023-12-06 14:53:32 +0000

    script assignment

---
+++ script.js
@@ -0,0 +1,55 @@
+// question 1
+let favouriteFruits = ["banana", "watermelon", "pineaple"];
+console.log(favouriteFruits[0])
+
+console.log(favouriteFruits[1])
+
+console.log(favouriteFruits[2])
+
+
+
+
+// question 2
+
+let JavaScriptFun = true
+
+console.log(`do you find javascript as fun or not? ${JavaScriptFun}`)
+
+
+// question3
+
+let num1 = 2;
+
+let num2 = 3;
+
+let sum = num1 + num2;
+
+console.log(sum)
+
+
+
+// question4
+
+let a = 5
+let b =10
+
+console.log(`let a =${a} and b=${b}`)
+
+
+
+// question5
+
+let firstName = "kod"
+let lastName = "Deviper"
+let fullName = "kod" + "Deviper"
+
+console.log(fullName)
+
+
+// question6
+
+const PI = 15
+const radius = 20
+const A =PI*radius^2
+
+console.log(A)
